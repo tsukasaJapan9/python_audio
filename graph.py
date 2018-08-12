0842e8b6b89e76966e4d2ec459b3191a93326e46
tsukasa <tsukasa.horinouchi@groove-x.com>
2018-08-12 11:44:18 +0900

    refactor

--- graph.py
+++ graph.py
@@ -2,44 +2,50 @@ import sys
 import time
 import os
 
-# x-axis
-max_hz = 8000
-div_hz = 80
-max_x = div_hz
-step_hz = max_hz / div_hz
 
-# y-axis
-max_amp = 50
-div_amp = 25
-max_y = div_amp
-step_amp = max_amp / div_amp
+class ImageDisp:
+    # x-axis
+    MAX_HZ = 8000
+    DIV_HZ = 80
 
-image_ary = [[0 for i in range(max_x)] for j in range(max_y)]
+    # y-axis
+    MAX_AMP = 50
+    DIV_AMP = 25
 
-def set_pixel(x, y, val):
-    image_ary[y][x] = val
+    MAX_X = DIV_HZ
+    MAX_Y = DIV_AMP
 
-def get_pixel(x, y):
-    return image_ary[y][x]
+    def __init__(self):
+        self.image_ary = \
+            [[0 for i in range(self.MAX_X)] for j in range(self.MAX_Y)]
 
-def disp_image():
-    os.system('clear')
-    for y in range(max_y):
-        line_x = ""
-        for x in range(max_x):
-            val = get_pixel(x, y)
-            if val == 0:
-                c = "0"
-            else:
-                c = "1"
-            line_x += c
-        print(line_x)
+    def set_pixel(self, x, y, val):
+        self.image_ary[y][x] = val
 
-print(image_ary)
+    def get_pixel(self, x, y):
+        return self.image_ary[y][x]
 
-for y in range(max_y):
-    set_pixel(2, y, 1)
+    def disp_image(self):
+        os.system('clear')
+        for y in range(self.MAX_Y):
+            line_x = ""
+            for x in range(self.MAX_X):
+                val = self.get_pixel(x, y)
+                if val == 0:
+                    c = "0"
+                else:
+                    c = "1"
+                line_x += c
+            print(line_x)
 
-for f in range(10):
-    disp_image()
-    time.sleep(0.5)
+
+def main():
+    image = ImageDisp()
+
+    for f in range(10):
+        image.disp_image()
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
